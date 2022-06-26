0f50800f3a11f820a597cbdf9157c28a3ee65c33
Alex P <ask@google.com>
2022-06-26 13:08:27 +0300

    variables renaming

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const periodSelectors = ["daily", "weekly", "monthly"];
 
-const statsElements = [ {current: "work",     previous: "work-previous" },
-                        {current: "play",     previous: "play-previous" },
-                        {current: "study",    previous: "study-previous"},
-                        {current: "exercise", previous: "exercise-previous"},
-                        {current: "social",   previous: "social-previous"},
-                        {current: "self",     previous: "self-previous"}];
+const hrsDisplays = [ {current: "work",     previous: "work-previous"    },
+                      {current: "play",     previous: "play-previous"    },
+                      {current: "study",    previous: "study-previous"   },
+                      {current: "exercise", previous: "exercise-previous"},
+                      {current: "social",   previous: "social-previous"  },
+                      {current: "self",     previous: "self-previous"    }];
 
-let highlitedSelector = "daily";    // tracks highlitedSelector and shows daily stats on page load
-data = JSON.parse(data);            // data comes from data.json (HTML <script src="./data.json">)
+let displaysNum = hrsDisplays.length
+let highlitedSelector = "daily"; // tracks highlitedSelector and shows daily stats on page load
+let stats = JSON.parse(data);    // data comes from data.json (HTML <script src="./data.json">)
 
 
 // period comes from clicked HTML element
@@ -17,12 +18,12 @@ function showStats(period) {
     updateHighlitedSelector(period);
 
     // show stats
-    for (let i = 0; i <= 5; i++) {
-        let currentHrs = data[i].timeframes[period].current;
-        let previousHrs = data[i].timeframes[period].previous;
+    for (let i = 0; i < displaysNum; i++) {
+        let currentHrs = stats[i].timeframes[period].current;
+        let previousHrs = stats[i].timeframes[period].previous;
 
-        let currHrsDisplay = document.getElementById(statsElements[i].current);
-        let prevHrsDisplay = document.getElementById(statsElements[i].previous);
+        let currHrsDisplay = document.getElementById(hrsDisplays[i].current);
+        let prevHrsDisplay = document.getElementById(hrsDisplays[i].previous);
 
         currHrsDisplay.innerHTML = currentHrs + getPostfix(currentHrs);
         prevHrsDisplay.innerHTML = getPrefix(period) + previousHrs + getPostfix(previousHrs);
