aa30e8347160ef433bbe93df187643f19f337874
Alex P <ask@google.com>
2022-06-26 12:56:41 +0300

    refactored JS code for readability and optimisations

--- script.js
+++ script.js
@@ -1,60 +1,39 @@
 const periodSelectors = ["daily", "weekly", "monthly"];
 
-const statsElements = [
-    {
-        current: "work",
-        previous: "work-previous"
-    },
-    {
-        current: "play",
-        previous: "play-previous"
-    },
-    {
-        current: "study",
-        previous: "study-previous"
-    },
-    {
-        current: "exercise",
-        previous: "exercise-previous"
-    },
-    {
-        current: "social",
-        previous: "social-previous"
-    },
-    {
-        current: "self",
-        previous: "self-previous"
-    }
-];
+const statsElements = [ {current: "work",     previous: "work-previous" },
+                        {current: "play",     previous: "play-previous" },
+                        {current: "study",    previous: "study-previous"},
+                        {current: "exercise", previous: "exercise-previous"},
+                        {current: "social",   previous: "social-previous"},
+                        {current: "self",     previous: "self-previous"}];
 
-// data comes from data.json (HTML <script src="./data.json">)
-data = JSON.parse(data);
+let highlitedSelector = "daily";    // tracks highlitedSelector and shows daily stats on page load
+data = JSON.parse(data);            // data comes from data.json (HTML <script src="./data.json">)
+
 
 // period comes from clicked HTML element
 function showStats(period) {
 
-    // highlight selected period selector
-    for (let selector of periodSelectors) {
+    updateHighlitedSelector(period);
 
-        let selectorElement = document.getElementById(selector);
-
-        if (selector == period) {
-            selectorElement.classList.add("period-selected");
-            continue;
-        }
-
-        selectorElement.classList.remove("period-selected");
-    }
-
-    // update stats
+    // show stats
     for (let i = 0; i <= 5; i++) {
         let currentHrs = data[i].timeframes[period].current;
         let previousHrs = data[i].timeframes[period].previous;
 
-        document.getElementById(statsElements[i].current).innerHTML = currentHrs + getPostfix(currentHrs);
-        document.getElementById(statsElements[i].previous).innerHTML = getPrefix(period) + previousHrs + getPostfix(previousHrs);
-      
+        let currHrsDisplay = document.getElementById(statsElements[i].current);
+        let prevHrsDisplay = document.getElementById(statsElements[i].previous);
+
+        currHrsDisplay.innerHTML = currentHrs + getPostfix(currentHrs);
+        prevHrsDisplay.innerHTML = getPrefix(period) + previousHrs + getPostfix(previousHrs);
     }
+}
+
+function updateHighlitedSelector(selectorToHighlight) {
+    if (selectorToHighlight == highlitedSelector) { return; }
+    document.getElementById(highlitedSelector).classList.remove("period-selected")
+    document.getElementById(selectorToHighlight).classList.add("period-selected");
+    highlitedSelector = selectorToHighlight;
 }
 
 function getPrefix(period) {
